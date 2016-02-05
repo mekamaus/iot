cc6c3794c72d651aece8c7e4daa9c728e9baa369
Sam Adams <samadams532@gmail.com>
2016-02-04 18:51:02 -0600

    see what samps range is

--- app/main.py
+++ app/main.py
@@ -10,12 +10,14 @@ import analyse
 # Initialize PyAudio
 pyaud = pyaudio.PyAudio()
 
+sense = sense_hat.SenseHat()
+
 # Open input stream, 16-bit mono at 44100 Hz
 # On my system, device 4 is a USB microphone
 stream = pyaud.open(
   format = pyaudio.paInt16,
   channels = 1,
-  rate = 8000,
+  rate = 48000,
   input_device_index = 3,
   input = True)
 
@@ -24,6 +26,11 @@ while True:
   rawsamps = stream.read(1024)
   # Convert raw data to NumPy array
   samps = numpy.fromstring(rawsamps, dtype=numpy.int16)
-  print(len(samps))
+
+  print(max(samps), min(samps))
+  #pixels = [samps[((((i % 8) - 3.5) ** 2 + ((i / 8) - 3.5) ** 2) ** 0.5) * 1024 / (3.5 * 2 ** 0.5 + 0.001)] for i in range(64)]
+
+  #sense.set_pixels(pixels)
+
   # Show the volume and pitch
   #print(analyse.loudness(samps), analyse.musical_detect_pitch(samps))
