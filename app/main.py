d7ff8294cf66617def06aa7b747d3f3f830bf88f
Sam Adams <samadams532@gmail.com>
2016-02-07 14:44:26 -0600

    try js

--- app/main.py
+++ app/main.py
@@ -1,68 +1,70 @@
-from sense_hat import SenseHat
-from random import randint
-from time import sleep
-import numpy
-import pyaudio
-import analyse
-import pyaudio
-import analyse
+#!/usr/bin/python
+# from sense_hat import SenseHat
+# from random import randint
+# from time import sleep
+# import numpy
+# import pyaudio
+# import analyse
+# import pyaudio
+# import analyse
 
-audio_rate = 44100
-num_samples = 8192
-
-# Initialize PyAudio
-pyaud = pyaudio.PyAudio()
-
-sense = SenseHat()
-
-# Open input stream, 16-bit mono at 44100 Hz
-# On my system, device 4 is a USB microphone
-stream = pyaud.open(
-  format = pyaudio.paInt16,
-  channels = 1,
-  rate = audio_rate,
-  input_device_index = 4,
-  input = True)
-
-min_samp = -512
-max_samp = 512
-
-while True:
-  # Read raw microphone data
-  try:
-    rawsamps = stream.read(8192)
-  except IOError:
-    print('Overflow')
-  # Convert raw data to NumPy array
-  samps = numpy.fromstring(rawsamps, dtype=numpy.int16)
-  print(len(samps))
-  if len(samps) == 0:
-    continue
-
-  distances = [
-    ((((i % 8) - 3.5) ** 2 + ((i / 8) - 3.5) ** 2) ** 0.5)
-    for i in range(64)
-  ]
-  indices = [
-    dist * len(samps) / (3.5 * 2 ** 0.5 + 0.001)
-    for dist in distances
-  ]
-  values = [
-    samps[index]
-    for index in indices
-  ]
-  pixels = [
-    (
-      max(min(255 * (v - min_samp) / (max_samp - min_samp), 255), 0),
-      max(min(255 * (v - min_samp) / (max_samp - min_samp), 255), 0),
-      max(min(255 * (v - min_samp) / (max_samp - min_samp), 255), 0)
-    )
-    for v in values
-  ]
-
-  sense.set_pixels(pixels)
+# audio_rate = 44100
+# num_samples = 8192
+#
+# # Initialize PyAudio
+# pyaud = pyaudio.PyAudio()
+#
+# sense = SenseHat()
+#
+# # Open input stream, 16-bit mono at 44100 Hz
+# # On my system, device 4 is a USB microphone
+# stream = pyaud.open(
+#   format = pyaudio.paInt16,
+#   channels = 1,
+#   rate = audio_rate,
+#   input_device_index = 4,
+#   input = True)
+#
+# min_samp = -512
+# max_samp = 512
+#
+# while True:
+#   # Read raw microphone data
+#   try:
+#     rawsamps = stream.read(8192)
+#   except IOError:
+#     print('Overflow')
+#   # Convert raw data to NumPy array
+#   samps = numpy.fromstring(rawsamps, dtype=numpy.int16)
+#   print(len(samps))
+#   if len(samps) == 0:
+#     continue
+#
+#   distances = [
+#     ((((i % 8) - 3.5) ** 2 + ((i / 8) - 3.5) ** 2) ** 0.5)
+#     for i in range(64)
+#   ]
+#   indices = [
+#     dist * len(samps) / (3.5 * 2 ** 0.5 + 0.001)
+#     for dist in distances
+#   ]
+#   values = [
+#     samps[index]
+#     for index in indices
+#   ]
+#   pixels = [
+#     (
+#       max(min(255 * (v - min_samp) / (max_samp - min_samp), 255), 0),
+#       max(min(255 * (v - min_samp) / (max_samp - min_samp), 255), 0),
+#       max(min(255 * (v - min_samp) / (max_samp - min_samp), 255), 0)
+#     )
+#     for v in values
+#   ]
+#
+#   sense.set_pixels(pixels)
 
   #print(analyse.loudness(samps), analyse.musical_detect_pitch(samps))
+
 
 
 # from picamera import PiCamera
