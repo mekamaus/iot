e6c30af7130efaa869ec1a1304aabf0fba74b625
Sam Adams <samadams532@gmail.com>
2016-03-14 23:18:22 -0500

    fixed ordering

--- index.js
+++ index.js
@@ -34,15 +34,21 @@ if (!a.isPresent()) {
 var fb = devname(a.get());
 console.log('Found framebuffer ' + fb);
 
-console.log('Pixel (0,0) = ' + getPixel(fb, 0, 0));
-for (var n = 1; --n >= 0;) {
+var pos = function (x, y) {
+  return 2 * (y * 8 + x);
+};
+
+var clear = function (fb) {
   for (var y = 8; --y >= 0;) {
     for (var x = 8; --x >= 0;) {
-      setPixel(fb, x, y, [random(0, 255), random(0, 255), random(0, 255)]);
+      setPixel(fb, x, y, [0, 0, 0]);
     }
   }
-}
-console.log('Pixel (0,0) = ' + getPixel(fb, 0, 0));
+};
+
+var random = function (low, high) {
+  return Math.floor(Math.random() * (high - low) + low);
+};
 
 var unpack = function (n) {
   var r = (n & 0xF800) >> 11;
@@ -60,10 +66,6 @@ var pack = function (rgb) {
   var b = (rgb[2] >> 3) & 0x1F;
   var bits = (r << 11) + (g << 5) + b;
   return bits;
-};
-
-var pos = function (x, y) {
-  return 2 * (y * 8 + x);
 };
 
 var getPixel = function (fb, x, y) {
@@ -94,17 +96,15 @@ var setPixel = function (fb, x, y, rgb) {
   fs.closeSync(fd);
 };
 
-var clear = function (fb) {
+console.log('Pixel (0,0) = ' + getPixel(fb, 0, 0));
+for (var n = 1; --n >= 0;) {
   for (var y = 8; --y >= 0;) {
     for (var x = 8; --x >= 0;) {
-      setPixel(fb, x, y, [0, 0, 0]);
+      setPixel(fb, x, y, [random(0, 255), random(0, 255), random(0, 255)]);
     }
   }
-};
-
-var random = function (low, high) {
-  return Math.floor(Math.random() * (high - low) + low);
-};
+}
+console.log('Pixel (0,0) = ' + getPixel(fb, 0, 0));
 
 var sense = {
   namefile: namefile,
