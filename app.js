4f8e0a491815c7f72ef178c30ec71f06c934ee6a
Sam Adams <samadams532@gmail.com>
2017-05-06 01:31:36 -0700

    asdf

---
+++ app.js
@@ -0,0 +1,13 @@
+import sense from 'rpi-sense-hat';
+import glob from 'glob';
+import color from 'color';
+//import hid from 'node-hid';
+
+//console.log(hid.devices());
+
+var updateDisplay = a => sense.setPixels((x, y) => {
+  const dist2 = (x - 3.5) * (x - 3.5) + (y - 3.5) * (y - 3.5);
+  const fac = .35 - .25 * a;
+  const intensity = Math.exp - fac * dist2;
+  return color('red').lightness(intensity).rgbArray();
+});
