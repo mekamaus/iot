b8d1f4b3830bf7277a92dfe05903a9f67e6fe386
Sam Adams <samadams532@gmail.com>
2017-05-05 19:42:52 -0700

    stuff'

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-import sense from 'rpi-sense-hat';
-import glob from 'glob';
-import color from 'color';
-//import hid from 'node-hid';
-
-//console.log(hid.devices());
-
-var updateDisplay = a => sense.setPixels((x, y) => {
-  const dist2 = (x - 3.5) * (x - 3.5) + (y - 3.5) * (y - 3.5);
-  const fac = .35 - .25 * a;
-  const intensity = Math.exp -fac * dist2;
-  color('red').lightness(intensity).rgbArray();
-});
+const path = require('path');j
+require('babel-register');
+require('babel-polyfill');
+require(path.join(__dirname, 'app.js'));
